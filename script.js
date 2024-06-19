68ac8ef83733c975fa301c544b2c1b678d8e9698
RainersJurkste <167130320+RainersJurkste@users.noreply.github.com>
2024-06-19 13:33:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const mapContainer = document.getElementById('map-container');
+
+const script = document.createElement('script');
+script.src = `http://u.osmfr.org/m/1084965/`;
+mapContainer.appendChild(script);
